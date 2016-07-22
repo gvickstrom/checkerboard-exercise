857a472ab4121b2b6db88fb79b3de971d81e396a
gvickstrom <gvickstrom@gmail.com>
2016-07-22 15:57:25 -0600

    updated the gradient file so that it works

--- src/gradient.js
+++ src/gradient.js
@@ -8,8 +8,7 @@ for (var j = 0; j < 9; j++) {
       document.body.appendChild(div)
       div.style.width = '11.1%';
       div.style.paddingBottom = '11.1%';
-      div.style.backgroundColor = "linear-gradient(top, #FF0000)";
-      //need to fix gradient
+      div.style.background = 'linear-gradient(0deg, red, yellow )';
       div.style.float = 'left';
     }
     else{
@@ -17,7 +16,7 @@ for (var j = 0; j < 9; j++) {
       document.body.appendChild(div)
       div.style.width = '11.1%';
       div.style.paddingBottom = '11.1%';
-      div.style.backgroundColor = "linear-gradient(top, #FFFF0000)";
+      div.style.background = 'linear-gradient(0deg, yellow, red)';
       //need to fix gradient
       div.style.float = 'left';
     }
